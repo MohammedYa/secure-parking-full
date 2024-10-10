0e9b497f3e1efdeb5f7ab14c438abb8ed438be5e
Magdy Elsayyad <magdy.elsayed@amcde.com>
2024-10-10 20:15:54 +0300

    uu

--- AngularParking.Server/obj/Release/net8.0/AngularParking.Server.AssemblyInfo.cs
+++ AngularParking.Server/obj/Release/net8.0/AngularParking.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AngularParking.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f644ed66190c0638f47459940a42e35b26d127e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+846b1855b1d624a5fd801ab3feb5398600aadd49")]
 [assembly: System.Reflection.AssemblyProductAttribute("AngularParking.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AngularParking.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
